e1bdb3908a2c048c368b9e3664d4c172ee3cd583
ziora-lawrence <danieliwuji28@gmail.com>
2026-08-28 23:46:11 +0100

    post setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-
+import Nav from './components/Nav';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Nav />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
       
     </>
   )
